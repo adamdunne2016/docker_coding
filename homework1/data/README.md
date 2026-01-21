6d49b67b646a06d3c9deab1999a060412e4f0bb4
Adam Dunne <44343355+adamdunne2016@users.noreply.github.com>
2026-01-21 15:07:36 +0000

    added green data

--- homework1/data/README.md
+++ homework1/data/README.md
@@ -14,13 +14,12 @@ wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parq
 
 docker run -it --rm \
   --network=data_default \
+  -v /workspaces/docker_coding:/data \
   green_ingest:v001 \
   --pg-user=root \
   --pg-pass=root \
   --pg-host=pgdatabase \
   --pg-port=5432 \
-  --year=2025 \
-  --month=11 \
-  --target-table=green_taxi_trips_2025_11 \
   --pg-db=ny_taxi \
+  --target-table=green_taxi_trips_2025_11 \
   --chunksize=100000
--- homework1/data/ingest_green_data.py
+++ homework1/data/ingest_green_data.py
@@ -41,40 +41,23 @@ parse_dates = [
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
 @click.option('--year', default=2021, type=int, help='Year of the data')
 @click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--target-table', default='green_taxi_data', help='Target table name')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     """Ingest NYC taxi data into PostgreSQL database."""
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-
+    url = '/data/green_tripdata_2025-11.parquet'
+    
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize,
+        # Read full parquet (for large files, implement chunked read)
+    df = pd.read_parquet(url)
+
+    df.to_sql(
+        name=target_table,
+        con=engine,
+        if_exists='replace',
+        index=False
     )
-
-    first = True
-
-    for df_chunk in tqdm(df_iter):
-        if first:
-            df_chunk.head(0).to_sql(
-                name=target_table,
-                con=engine,
-                if_exists='replace'
-            )
-            first = False
-
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists='append'
-        )
-
 
 if __name__ == '__main__':
     run()
